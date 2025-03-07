72cc82e61e71011c94e18085eeeb91167a825d76
Oleh Liubichenko <oleh.liubichenko@gmail.com>
2025-03-07 02:12:52 +0200

    Make turtle put random color dots proportionally on screen

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from turtle import Turtle, Screen, colormode
+from random import choice
 import colorgram
 
+colormode(255)
 colors = colorgram.extract("hirsty.jpeg", 30)
 formatted_colors = []
 
@@ -9,3 +12,25 @@ for color in colors:
     b = color.rgb.b
     formatted_colors.append((r, b, g))
 
+jack = Turtle()
+jack.speed("fastest")
+jack.shape("turtle")
+jack.penup()
+jack.setpos(-275, -275)
+
+for _ in range(1, 101):
+    jack.color(choice(formatted_colors))
+    jack.pendown()
+    jack.dot(30)
+    jack.penup()
+    jack.forward(60)
+    if _ % 10 == 0:
+        jack.left(90)
+        jack.forward(60)
+        jack.left(90)
+        jack.forward(600)
+        jack.left(180)
+
+
+screen = Screen()
+screen.exitonclick()
